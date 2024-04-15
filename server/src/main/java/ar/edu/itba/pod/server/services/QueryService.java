341fba4b651af335f98691fb797c7290b3a90211
Santiago Ballerini <sballerini@itba.edu.ar>
2024-04-15 14:16:11 -0300

    feat: implement Checkins gRPC method call for query service

--- server/src/main/java/ar/edu/itba/pod/server/services/QueryService.java
+++ server/src/main/java/ar/edu/itba/pod/server/services/QueryService.java
@@ -1,11 +1,15 @@
 package ar.edu.itba.pod.server.services;
 
 import ar.edu.itba.pod.grpc.query.*;
+import ar.edu.itba.pod.server.models.Checkin;
 import ar.edu.itba.pod.server.queues.PassengerQueue;
 import ar.edu.itba.pod.server.repositories.CheckinRepository;
 import ar.edu.itba.pod.server.repositories.CounterRepository;
 
 import io.grpc.stub.StreamObserver;
+
+import java.util.List;
+import java.util.function.Predicate;
 
 public class QueryService extends QueryServiceGrpc.QueryServiceImplBase {
 
@@ -24,7 +28,40 @@ public class QueryService extends QueryServiceGrpc.QueryServiceImplBase {
     }
 
     @Override
-    public void checkins(CheckinsRequest request, StreamObserver<CheckinsResponse> responseObserver) {}
+    public void checkins(
+            CheckinsRequest request, StreamObserver<CheckinsResponse> responseObserver) {
+
+        String sector = request.getSectorName();
+        String airline = request.getAirline();
+
+        Predicate<Checkin> predicate = checkin -> true;
+
+        if (!sector.isEmpty()) {
+            predicate = predicate.and(checkin -> checkin.sector().equals(sector));
+        }
+
+        if (!airline.isEmpty()) {
+            predicate = predicate.and(checkin -> checkin.airline().equals(airline));
+        }
+
+        List<CheckinInfo> checkins =
+                checkinRepository.getCheckins(predicate).stream()
+                        .map(
+                                checkin ->
+                                        CheckinInfo.newBuilder()
+                                                .setSectorName(checkin.sector())
+                                                .setCounter(checkin.counter())
+                                                .setAirline(checkin.airline())
+                                                .setFlight(checkin.flight())
+                                                .setBooking(checkin.booking())
+                                                .build())
+                        .toList();
+
+        CheckinsResponse response = CheckinsResponse.newBuilder().addAllCheckins(checkins).build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 
     @Override
     public void counters(
