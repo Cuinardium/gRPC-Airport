8607e8aef76a1a2e83dd5a3b8aed83aa49110412
Santiago Ballerini <sballerini@itba.edu.ar>
2024-04-18 18:09:07 -0300

    feat: implement checkin repository

--- server/src/main/java/ar/edu/itba/pod/server/repositories/CheckinRepositoryImpl.java
+++ server/src/main/java/ar/edu/itba/pod/server/repositories/CheckinRepositoryImpl.java
@@ -2,36 +2,46 @@ package ar.edu.itba.pod.server.repositories;
 
 import ar.edu.itba.pod.server.models.Checkin;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.function.Predicate;
 
 public class CheckinRepositoryImpl implements CheckinRepository {
+
+    private final Collection<Checkin> checkins;
+
+    public CheckinRepositoryImpl() {
+        this.checkins = new ConcurrentLinkedQueue<>();
+    }
+
     @Override
-    public void addCheckin(Checkin checkin) {}
+    public void addCheckin(Checkin checkin) {
+        checkins.add(checkin);
+    }
 
     @Override
     public List<Checkin> getCheckins() {
-        return List.of();
+        return getCheckins((checkin -> true));
     }
 
     @Override
     public List<Checkin> getCheckins(Predicate<Checkin> predicate) {
-        return List.of();
+        Checkin[] checkinsArray = checkins.toArray(new Checkin[0]);
+        return Arrays.stream(checkinsArray).filter(predicate).toList();
     }
 
     @Override
     public Optional<Checkin> getCheckin(String booking) {
-        return Optional.empty();
+        return checkins.stream().filter(checkin -> checkin.booking().equals(booking)).findFirst();
     }
 
     @Override
     public boolean hasCheckins() {
-        return false;
+        return !checkins.isEmpty();
     }
 
     @Override
     public boolean hasCheckin(String booking) {
-        return false;
+        return getCheckin(booking).isPresent();
     }
 }
