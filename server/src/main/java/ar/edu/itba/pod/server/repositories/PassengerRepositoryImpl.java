e83c30aaa5edae05293a351783243c2089e26278
Santiago Ballerini <sballerini@itba.edu.ar>
2024-04-18 18:09:20 -0300

    feat: implement passenger repository

--- server/src/main/java/ar/edu/itba/pod/server/repositories/PassengerRepositoryImpl.java
+++ server/src/main/java/ar/edu/itba/pod/server/repositories/PassengerRepositoryImpl.java
@@ -2,26 +2,40 @@ package ar.edu.itba.pod.server.repositories;
 
 import ar.edu.itba.pod.server.models.Passenger;
 
+import java.util.Collection;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class PassengerRepositoryImpl implements PassengerRepository {
+
+    private final Collection<Passenger> expectedPassengers;
+
+    public PassengerRepositoryImpl() {
+        this.expectedPassengers = new ConcurrentLinkedQueue<>();
+    }
+
     @Override
     public boolean hasAirline(String airline) {
-        return false;
+        return expectedPassengers.stream()
+                .filter(passenger -> passenger.airline().equals(airline))
+                .findAny()
+                .isEmpty();
     }
 
     @Override
     public boolean hasPassenger(Passenger passenger) {
-        return false;
+        return expectedPassengers.contains(passenger);
     }
 
     @Override
     public Optional<Passenger> getPassenger(String booking) {
-        return Optional.empty();
+        return expectedPassengers.stream()
+                .filter(passenger -> passenger.booking().equals(booking))
+                .findFirst();
     }
 
     @Override
     public void addPassenger(Passenger passenger) {
-
+        expectedPassengers.add(passenger);
     }
 }
