5cdd4af4b61e8cd621fe9f80e306b4078daa0d55
Franco Bosetti <francobosetti@live.com.ar>
2024-04-19 12:26:03 -0300

    feat: listSectors in counter service

--- server/src/main/java/ar/edu/itba/pod/server/services/CounterService.java
+++ server/src/main/java/ar/edu/itba/pod/server/services/CounterService.java
@@ -1,13 +1,22 @@
 package ar.edu.itba.pod.server.services;
 
+import ar.edu.itba.pod.grpc.common.CounterRange;
 import ar.edu.itba.pod.grpc.counter.*;
 import ar.edu.itba.pod.server.events.EventManager;
+import ar.edu.itba.pod.server.models.CountersRange;
+import ar.edu.itba.pod.server.models.Range;
 import ar.edu.itba.pod.server.repositories.CheckinRepository;
 import ar.edu.itba.pod.server.repositories.CounterRepository;
 import ar.edu.itba.pod.server.repositories.PassengerRepository;
 import com.google.protobuf.Empty;
 
 import io.grpc.stub.StreamObserver;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CounterService extends CounterServiceGrpc.CounterServiceImplBase {
 
@@ -31,12 +40,16 @@ public class CounterService extends CounterServiceGrpc.CounterServiceImplBase {
     @Override
     public void listPendingAssignments(
             ListPendingAssignmentsRequest request,
-            StreamObserver<ListPendingAssignmentsResponse> responseObserver) {}
+            StreamObserver<ListPendingAssignmentsResponse> responseObserver) {
+
+    }
 
     @Override
     public void checkinCounters(
             CheckinCountersRequest request,
-            StreamObserver<CheckinCountersResponse> responseObserver) {}
+            StreamObserver<CheckinCountersResponse> responseObserver) {
+
+    }
 
     @Override
     public void freeCounters(
@@ -53,5 +66,49 @@ public class CounterService extends CounterServiceGrpc.CounterServiceImplBase {
 
     @Override
     public void listSectors(
-            Empty request, StreamObserver<ListSectorsResponse> responseObserver) {}
+            Empty request, StreamObserver<ListSectorsResponse> responseObserver) {
+        List<CountersRange> countersRanges = counterRepository.getCounters();
+
+        List<SectorInfo> sectorsInfoList = generateSectorInfoList(countersRanges);
+
+        ListSectorsResponse response =
+                ListSectorsResponse.newBuilder()
+                        .addAllSectors(sectorsInfoList)
+                        .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+    //TODO: REVISE THIS
+    private List<SectorInfo> generateSectorInfoList(List<CountersRange> countersRanges) {
+        Map<String, List<CountersRange>> sectorsMap = new HashMap<>();
+
+        for (CountersRange countersRange : countersRanges) {
+            String sector = countersRange.sector();
+            if (!sectorsMap.containsKey(sector)) {
+                sectorsMap.put(sector, new ArrayList<>());
+            }
+            sectorsMap.get(sector).add(countersRange);
+        }
+
+        List<SectorInfo> sectorInfoList = new ArrayList<>();
+        for(Map.Entry<String, List<CountersRange>> entry : sectorsMap.entrySet()) {
+            String sector = entry.getKey();
+            List<CounterRange> counterRangesList = entry.getValue().stream().map((countersRange -> {
+                Range range = countersRange.range();
+                return CounterRange.newBuilder()
+                                .setFrom(range.from())
+                                .setTo(range.to())
+                                .build();
+            })).collect(Collectors.toList());
+
+            SectorInfo sectorInfo =
+                    SectorInfo.newBuilder()
+                            .setSectorName(sector)
+                            .addAllCounterRanges(counterRangesList)
+                            .build();
+            sectorInfoList.add(sectorInfo);
+        }
+        return sectorInfoList;
+    }
 }
