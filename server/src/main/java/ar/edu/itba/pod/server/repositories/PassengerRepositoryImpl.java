86aa64fb1ac5a408cb62719ac67095a6b47c7f5a
Santiago Ballerini <sballerini@itba.edu.ar>
2024-04-19 14:22:00 -0300

    feat: change passenger repository implementation to use a concurrent has map

--- server/src/main/java/ar/edu/itba/pod/server/repositories/PassengerRepositoryImpl.java
+++ server/src/main/java/ar/edu/itba/pod/server/repositories/PassengerRepositoryImpl.java
@@ -1,44 +1,41 @@
 package ar.edu.itba.pod.server.repositories;
 
+import ar.edu.itba.pod.server.exceptions.AlreadyExistsException;
 import ar.edu.itba.pod.server.models.Passenger;
 
-import java.util.Collection;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class PassengerRepositoryImpl implements PassengerRepository {
 
-    private final Collection<Passenger> expectedPassengers;
+    private final ConcurrentHashMap<String, Passenger> expectedPassengers;
 
     public PassengerRepositoryImpl() {
-        this.expectedPassengers = new ConcurrentLinkedQueue<>();
+        this.expectedPassengers = new ConcurrentHashMap<>();
     }
 
     @Override
     public boolean hasAirline(String airline) {
-        return expectedPassengers.stream()
+        return expectedPassengers.values().stream()
                 .anyMatch(passenger -> passenger.airline().equals(airline));
     }
 
     @Override
     public boolean hasPassenger(Passenger passenger) {
-        return expectedPassengers.contains(passenger);
+        return getPassenger(passenger.booking()).isPresent();
     }
 
     @Override
     public Optional<Passenger> getPassenger(String booking) {
-        return expectedPassengers.stream()
-                .filter(passenger -> passenger.booking().equals(booking))
-                .findFirst();
+        return Optional.ofNullable(expectedPassengers.get(booking));
     }
 
     @Override
-    public void addPassenger(Passenger passenger) {
-        expectedPassengers.add(passenger);
     public void addPassenger(Passenger passenger) throws AlreadyExistsException {
         if (hasPassenger(passenger)) {
             throw new AlreadyExistsException("Passenger already exists");
         }
 
+        expectedPassengers.put(passenger.booking(), passenger);
     }
 }
