6cff947d7e2dd051ba23ec89e62397f606a32348
Santiago Ballerini <sballerini@itba.edu.ar>
2024-04-27 16:24:55 -0300

    fix: fix assembly client scripts

--- client/src/main/assembly/overlay/adminClient.sh
+++ client/src/main/assembly/overlay/adminClient.sh
@@ -2,9 +2,11 @@
 
 PATH_TO_CODE_BASE=`pwd`
 
-JAVA_OPTS="-Djava.security.debug=access -Djava.security.manager -Djava.security.policy=/$PATH_TO_CODE_BASE/java.policy -Djava.rmi.server.useCodebaseOnly=false"
+#JAVA_OPTS="-Djava.security.debug=access -Djava.security.manager -Djava.security.policy=/$PATH_TO_CODE_BASE/java.policy -Djava.rmi.server.useCodebaseOnly=false"
+# Add arguments as java opts
+JAVA_OPTS="$JAVA_OPTS $*"
 
 MAIN_CLASS="ar.edu.itba.pod.client.AdminClient"
 
 
-java  "$JAVA_OPTS" -cp 'lib/jars/*' $MAIN_CLASS "$*"
+java  $JAVA_OPTS -cp 'lib/jars/*' $MAIN_CLASS
--- client/src/main/assembly/overlay/passengerClient.sh
+++ client/src/main/assembly/overlay/passengerClient.sh
@@ -2,9 +2,9 @@
 
 PATH_TO_CODE_BASE=`pwd`
 
-JAVA_OPTS="-Djava.security.debug=access -Djava.security.manager -Djava.security.policy=/$PATH_TO_CODE_BASE/java.policy -Djava.rmi.server.useCodebaseOnly=false"
+#JAVA_OPTS="-Djava.security.debug=access -Djava.security.manager -Djava.security.policy=/$PATH_TO_CODE_BASE/java.policy -Djava.rmi.server.useCodebaseOnly=false"
 
 MAIN_CLASS="ar.edu.itba.pod.client.PassengerClient"
 
 
-java  "$JAVA_OPTS" -cp 'lib/jars/*' $MAIN_CLASS "$*"
+java  $JAVA_OPTS -cp 'lib/jars/*' $MAIN_CLASS "$*"
