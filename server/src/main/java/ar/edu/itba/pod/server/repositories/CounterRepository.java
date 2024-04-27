c294328ba21ad8a430f581c8aed8c96bd99c2c88
Santiago Ballerini <sballerini@itba.edu.ar>
2024-04-27 17:46:12 -0300

    refactor: reorder counter repository methods

--- server/src/main/java/ar/edu/itba/pod/server/repositories/CounterRepository.java
+++ server/src/main/java/ar/edu/itba/pod/server/repositories/CounterRepository.java
@@ -5,7 +5,6 @@ import ar.edu.itba.pod.server.exceptions.HasPendingPassengersException;
 import ar.edu.itba.pod.server.exceptions.UnauthorizedException;
 import ar.edu.itba.pod.server.models.*;
 import ar.edu.itba.pod.server.utils.Pair;
-
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -14,27 +13,32 @@ public interface CounterRepository {
     // Lists sorted by range
     List<CountersRange> getCounters();
     List<CountersRange> getCountersFromSector(String sector);
-    Optional<CountersRange> getFlightCounters(String flight);
-    Optional<Pair<CountersRange, String>> getFlightCountersAndSector(String flight);
-    List<PendingAssignment> getQueuedAssignments(String sector);
+
+
+    // ----- Sectors -----
+    void addSector(String sector) throws AlreadyExistsException;
+    boolean hasSector(String sector);
     List<Sector> getSectors();
     Optional<Sector> getSector(String sectorName);
-    List<String> getPreviouslyAssignedFlights();
 
-    void assignCounterRange(String sector, CountersRange counterRange);
-
-    void removeAssignmentFromQueue(String sector, PendingAssignment assignment);
-
-    boolean hasCounters();
-    boolean hasSector(String sector);
-    boolean hasPassengerInCounter(Range counterRange, String booking);
-
-    void addSector(String sector) throws AlreadyExistsException;
+    // ----- Counters -----
     Range addCounters(String sector, int counterCount) throws NoSuchElementException;
-    int addPassengerToQueue(Range counterRange, String booking) throws AlreadyExistsException;
-    void addAssignmentToQueue(String sector, PendingAssignment assignment);
+    boolean hasCounters();
+    Optional<CountersRange> getFlightCounters(String flight);
+    Optional<Pair<CountersRange, String>> getFlightCountersAndSector(String flight);
 
+    // ----- Assignments -----
+    void assignCounterRange(String sector, CountersRange counterRange);
+    List<String> getPreviouslyAssignedFlights();
     List<CountersRange> freeCounters(String sector, int counterFrom, String airline) throws NoSuchElementException, HasPendingPassengersException;
 
+    // ----- Queues - Assignments -----
+    void addAssignmentToQueue(String sector, PendingAssignment assignment);
+    void removeAssignmentFromQueue(String sector, PendingAssignment assignment);
+    List<PendingAssignment> getQueuedAssignments(String sector);
+
+    // ----- Queues - Passengers -----
+    boolean hasPassengerInCounter(Range counterRange, String booking);
+    int addPassengerToQueue(Range counterRange, String booking) throws AlreadyExistsException;
     List<Optional<String>> checkinCounters(String sector, int counterFrom, String airline) throws NoSuchElementException, UnauthorizedException;
 }
