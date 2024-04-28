2890cb6723b189ce8607477eb56b7e350ffaa389
Santiago Ballerini <sballerini@itba.edu.ar>
2024-04-27 22:21:28 -0300

    fix: fix assembly scripts

--- client/src/main/assembly/overlay/eventsClient.sh
+++ client/src/main/assembly/overlay/eventsClient.sh
@@ -3,6 +3,8 @@
 PATH_TO_CODE_BASE=`pwd`
 
 #JAVA_OPTS="-Djava.security.debug=access -Djava.security.manager -Djava.security.policy=/$PATH_TO_CODE_BASE/java.policy -Djava.rmi.server.useCodebaseOnly=false"
+# Add arguments as java opts
+JAVA_OPTS="$JAVA_OPTS $*"
 
 MAIN_CLASS="ar.edu.itba.pod.client.EventsClient"
 
