4f26805786bf67c485ebc78b41e8820837f251ef
Santiago Ballerini <sballerini@itba.edu.ar>
2024-04-28 15:01:34 -0300

    fix: add constructor to assignments if no callback are needed

--- server/src/main/java/ar/edu/itba/pod/server/models/Assignment.java
+++ server/src/main/java/ar/edu/itba/pod/server/models/Assignment.java
@@ -2,4 +2,9 @@ package ar.edu.itba.pod.server.models;
 
 import java.util.List;
 
-public record Assignment(String airline, List<String> flights, int counterCount, Runnable onMoved, Runnable onAssigned) {}
+public record Assignment(String airline, List<String> flights, int counterCount, Runnable onMoved, Runnable onAssigned) {
+
+    public Assignment(String airline, List<String> flights, int counterCount) {
+        this(airline, flights, counterCount, () -> {}, () -> {});
+    }
+}
